7c2ca102895e91b6759d25ac95f56b8d0b43742c
lmotta <motta.luiz@gmail.com>
2016-08-07 21:18:50 -0300

    Add name image in argument of program

--- testworker.py
+++ testworker.py
@@ -7,24 +7,24 @@ from optparse import OptionParser
 
 from workerprocessingimage import WorkerLocalImage, WorkerPLScene
 
-def setWorkerPLScene():
+def setWorkerPLScene(name_image):
   WorkerPLScene.isKilled = False
   image = {
-    'name': '20160421_110213_0c53',
+    'name': name_image,
     'PRODUCT_TYPE': "analytic"
   }
   idWorker = 1
   return ( WorkerPLScene( idWorker ), image )
 
-def setWorkerLocal():
+def setWorkerLocal(name_image):
   WorkerLocalImage.isKilled = False
   image = {
-    'name': '20160421_110213_0c53.tif'
+    'name': name_image
   }
   idWorker = 1
   return ( WorkerLocalImage( idWorker ), image )
 
-def run(type_worker):
+def run(type_worker, name_image):
   def printTime(title, t1=None):
     tn =datetime.datetime.now() 
     st = tn.strftime('%Y-%m-%d %H:%M:%S')
@@ -45,7 +45,7 @@ def run(type_worker):
     return { 'isOk': isOk, 'msg': msg }
 
   set_worker = { 'local': setWorkerLocal, 'pl': setWorkerPLScene }
-  ( worker, image ) = set_worker[ type_worker ]()
+  ( worker, image ) = set_worker[ type_worker ]( name_image )
 
   printTime( "Setting Dataset '%s'(worker %s)" % ( image['name'], type_worker ) )
   vreturn = worker.setImage( image )
@@ -63,23 +63,20 @@ def run(type_worker):
     print "Error: %s" % vreturn['msg']
 
 def main():
-  usage = "usage: %prog type_worker"
+  usage = "usage: %prog type_worker name_image"
   parser = OptionParser(usage)
-
-  # define options
   (opts, args) = parser.parse_args()
 
-  if len(sys.argv) == 1:
-    parser.print_help()
-    return 1
-  elif len(args) == 0:
+  if not len(args) == 2:
     parser.print_help()
     return 1
   else:
     if not args[0] in ('local', 'pl'):
-      print("Type of workers: 'local' or 'pl'.")
+      parser.print_help()
+      print("Type of worker: 'local' or 'pl'.")
       return 1
-    return run( args[0] )
+    
+    return run( args[0], args[1] )
 
 if __name__ == "__main__":
     sys.exit( main() )
